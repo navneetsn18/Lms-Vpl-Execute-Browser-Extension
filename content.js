34e97164ce7eca5eb792565d021a3f7648581fe8
navneetsn18 <navneetsn18@gmail.com>
2020-04-03 17:35:05 +0530

    Commit

--- content.js
+++ content.js
@@ -7,18 +7,15 @@ function gotmessage(message, sender, sendResponse) {
             alert('Press ok to start!');
             document.getElementById("vpl_ide_evaluate").click();
             var trick = setInterval(function() {
-                if (document.querySelector("h4#ui-accordion-vpl_results_accordion-header-0.vpl_ide_accordion_t_grade.ui-accordion-header.ui-helper-reset.ui-state-default.ui-corner-all.ui-accordion-icons").textContent ===
-                    "Proposed grade: 100 / 100") {
+                var res = document.querySelector("h4#ui-accordion-vpl_results_accordion-header-0.vpl_ide_accordion_t_grade.ui-accordion-header.ui-helper-reset.ui-state-default.ui-corner-all.ui-accordion-icons").textContent;
+                res = res.slice(15, -5);
+                res = parseInt(res);
+                if (res == 100) {
                     stopInterval();
                     alert("Execution successful!");
-                } else if (document.querySelector("h4#ui-accordion-vpl_results_accordion-header-0.vpl_ide_accordion_t_grade.ui-accordion-header.ui-helper-reset.ui-state-default.ui-corner-all.ui-accordion-icons").textContent ===
-                    "Proposed grade: 50 / 100") {
+                } else if (res <= 99 && res >= 0) {
                     stopInterval();
                     alert("Please check your program again! Its need some more efforts!");
-                } else if (document.querySelector("h4#ui-accordion-vpl_results_accordion-header-0.vpl_ide_accordion_t_grade.ui-accordion-header.ui-helper-reset.ui-state-default.ui-corner-all.ui-accordion-icons").textContent ===
-                    "Proposed grade: 0 / 100") {
-                    stopInterval();
-                    alert("Please check your program again!");
                 } else { document.getElementById("vpl_ide_evaluate").click(); }
             }, 10000);
 
